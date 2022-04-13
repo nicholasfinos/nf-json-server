cb64fc8bd90b0c3a2accf51a6bbd81e6940c5075
nicholasfinos <nicholasfinos@gmail.com>
2022-04-13 14:16:36 +1000

    updated json files to match band data

--- db.js
+++ db.js
@@ -1,13 +1,7 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const bands = require("./data/bands");
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
+  bands
 }
 
 
