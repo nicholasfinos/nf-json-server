1441e3edfc517bffeb082895194fcf034695922d
nicholasfinos <nicholasfinos@gmail.com>
2022-04-19 11:51:10 +1000

    added farm collection

--- db.js
+++ db.js
@@ -1,30 +1,8 @@
 const bands = require("./data/bands");
+const farms = require("./data/farms");
 
 module.exports = {
-  bands
+  bands,
+  farms
 }
 
-
-// const items = require("./data/items");
-
-// module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
- 
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
-
-//   return {
-//     items,
-//     tags,
-//     brands,
-//     itemTypes,
-//   };
-// };
